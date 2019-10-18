73627fc3eb61f704c2ed8609deed51d80e498a43
Jeremy Schoffen <jschoffen.dev@gmail.com>
2019-10-18 18:00:19 +0200

    Ported the second part of the tests. The last test of the old ns won't be ported since we can't duplicated same level versions anymore.

--- test/metav/api/release_test.clj
+++ test/metav/api/release_test.clj
@@ -13,10 +13,13 @@
     [clojure.pprint :as pp]))
 
 
+;; TODO: can't sign tags in tests for now -> need to setup the environment so that gpg can be found by the java procees spawned with Runtime.exec
+;; TODO: Figure out how to use the git env from metav.git-shell/GIT_ENV when calling metav.git functions.
+;;       Right now, it's my git conf that's used instead.
 (deftest release-repo
   (testing "bump from a clean tagged repo, testing the spitted files"
     (ut/with-example-monorepo m
-      (let [{:keys [remote monorepo modules] :as mono} m
+      (let [{:keys [monorepo modules]} m
             {moduleA1 :A1} modules
 
             options {:metav.release/without-sign true
@@ -31,7 +34,7 @@
              bumped-tag :metav/tag
              prefix :metav/version-prefix} ctxt-after-release
 
-            [scm-base distance sha dirty?] (m-git/working-copy-description moduleA1 :prefix prefix)
+            [scm-base] (m-git/working-copy-description moduleA1 :prefix prefix)
             tag-verify (m-git/tag-verify monorepo bumped-tag)]
         (facts
           (str bumped-version) => "1.3.5"
@@ -40,6 +43,69 @@
           (:exit tag-verify) => 1
           (fs/exists? (str moduleA1 "/resources/metav/meta.edn")) => true
           (fs/exists? (str moduleA1 "/resources/metav/meta.clj")) => true
-          (fs/exists? (str moduleA1 "/resources/metav/meta.json")) => true)))))
+          (fs/exists? (str moduleA1 "/resources/metav/meta.json")) => true))))
 
-;; TODO: port the other tests
+  (testing "bump from a clean tagged repo, two patch releases, then one minor, then one major"
+    (ut/with-example-monorepo m
+      (let [{:keys [remote monorepo modules] :as mono} m
+            {moduleA2 :A2} modules
+
+            general-option {:metav.spit/formats         #{:edn}
+                            :metav.release/level        :patch
+                            :metav.release/spit         true
+                            :metav.release/without-sign true
+                            :metav.release/output-dir   "resources"
+                            :metav.release/namespace    "meta"}
+
+            release! (fn [level]
+                       (let [options (assoc general-option :metav.release/level level)
+                             ctxt-after-release (m-release/perform! (ut/make-context moduleA2 options))
+
+                             {bumped-version :metav/version
+                              bumped-tag     :metav/tag
+                              prefix         :metav/version-prefix} ctxt-after-release
+
+                             [scm-base] (m-git/working-copy-description moduleA2 :prefix prefix)]
+                         (Thread/sleep 500) ;need to wait because the time resolution of the git describe command needs some time to elapse before asking whether a new tag is available
+                         {:bumped-version bumped-version
+                          :bumped-tag bumped-tag
+                          :scm-base scm-base
+                          :tag-verify (m-git/tag-verify monorepo bumped-tag)
+                          :metadata (m-git/tag-message monorepo bumped-tag)}))
+
+            release1 (release! :patch)]
+
+        (fact
+          (release! :patch) =throws=> Exception) ;; no change :patch would duplicate
+
+        (gs/shell-in-dir! moduleA2
+          (gs/write-dummy-file-in! "src")
+          (gs/add!)
+          (gs/commit!))
+        (let [release2 (release! :patch)
+              release3 (release! :minor)
+              release4 (release! :major)]
+          (facts
+            (str (:bumped-version release1)) => "1.1.3"
+            (:scm-base release1)  => "1.1.3"
+            (:bumped-tag release1) => "sysA-container2-1.1.3"
+            (:tag-verify release1) => truthy
+            (:metadata release1)   => truthy
+            (fs/exists? (str moduleA2 "/resources/meta.edn")) => true
+
+            (str (:bumped-version release2)) => "1.1.4"
+            (:scm-base release2)  => "1.1.4"
+            (:bumped-tag release2) => "sysA-container2-1.1.4"
+            (:metadata release2)   => truthy
+
+            (str (:bumped-version release3)) => "1.2.0"
+            (:scm-base release3)  => "1.2.0"
+            (:bumped-tag release3) => "sysA-container2-1.2.0"
+            (:metadata release3)   => truthy
+
+            (str (:bumped-version release4)) => "2.0.0"
+            (:scm-base release4)  => "2.0.0"
+            (:bumped-tag release4) => "sysA-container2-2.0.0"
+            (:metadata release4)   => truthy))))))
+
+(test/run-tests)
