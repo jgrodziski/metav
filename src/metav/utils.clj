f9b658cb03165405e377fcad9c810332b3b5ddcf
Jeremy Schoffen <jschoffen.dev@gmail.com>
2019-11-01 18:09:01 +0100

    Reworked the assertion function not to use s/assert* and adding reporting via logger.

--- src/metav/utils.clj
+++ src/metav/utils.clj
@@ -3,6 +3,7 @@
     [clojure.spec.alpha :as s]
     [clojure.java.io :as io]
     [clojure.string :as string]
+    [clojure.tools.logging :as log]
     [me.raynes.fs :as fs]))
 
 
@@ -28,7 +29,21 @@
   (string/starts-with? (str (fs/normalized possible-descendant))
                        (str (fs/normalized path))))
 
-(def assert-spec s/assert*)
+
+(defn assert-spec [spec x]
+  (if (s/valid? spec x)
+    x
+    (let [ed (assoc (s/explain-data spec  x)
+               ::failure :assertion-failed)
+          msg (str "Spec assertion for spec `" spec "` failed\n"
+                   (with-out-str (s/explain-out ed)))
+          err (ex-info
+                msg
+                ed)]
+      (log/error err msg)
+      (throw err))))
+
+
 
 (defn merge&validate [opts defaults spec]
   (->> opts
