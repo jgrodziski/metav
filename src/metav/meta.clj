ae8a2359d611be2f8e97919607106f85992b7124
jeremiegrodziski <jeremie@grodziski.com>
2020-03-06 17:46:08 +0100

    Bump metav to 1.6.7 and spit metadata

--- src/metav/meta.clj
+++ src/metav/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "metav")
 (def path ".")
-(def version "1.6.6")
-(def tag "v1.6.6")
-(def generated-at "2020-02-26T23:02:22Z")
+(def version "1.6.7")
+(def tag "v1.6.7")
+(def generated-at "2020-03-06T16:46:08Z")
