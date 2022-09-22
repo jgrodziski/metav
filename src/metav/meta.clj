83dbd1fba42e868783a93c1e58b2a4d3c2a5055b
Jérémie Grodziski <jeremie@grodziski.com>
2022-09-22 16:05:57 +0200

    Bump metav to 1.7.1 and spit metadata

--- src/metav/meta.clj
+++ src/metav/meta.clj
@@ -3,6 +3,6 @@
 
 (def module-name "metav")
 (def path ".")
-(def version "1.7.0")
-(def tag "v1.7.0")
-(def generated-at "2022-09-22T13:57:20Z")
+(def version "1.7.1")
+(def tag "v1.7.1")
+(def generated-at "2022-09-22T14:05:57Z")
